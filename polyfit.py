b5fdd92cfa17dcd03a6c55ba0573739a110f2594
yuvalsolaz <yuval.solaz@gmail.com>
2019-02-24 18:53:40 +0200

    Add files via upload

---
+++ polyfit.py
@@ -0,0 +1,36 @@
+# !pip install descartes
+
+from __future__ import (absolute_import, division, print_function)
+
+from shapely.wkt import loads
+from shapely import affinity
+from geopandas import GeoSeries, GeoDataFrame
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots(figsize=(5, 5))
+
+
+def draw(shape,color='black'):
+    GeoSeries(shape).plot(ax=ax,color=color)
+    plt.pause(0.05)
+
+def xor(poly, rect='y'):
+    intersection = poly.intersection(rect)
+    nonoverlap = rect.difference(intersection)
+    return nonoverlap
+
+def loss(poly, rect):
+    return xor(poly,rect).area
+
+if __name__ == '__main__':
+    poly = loads('POLYGON((-150 100, -50 150, 0 100, 110 160, 150 0, 50 -50, -100 -90, -150 100))')
+    rect = loads('POLYGON((-100 -50, -100 50, 100 50, 100 -50, -100 -50))')
+    draw(poly,color='blue')
+    draw(rect,color='green')
+    x = xor(poly=poly, rect=rect)
+    draw(x)
+    plt.pause(5)
+
+    for i in range(90):
+        rls = affinity.rotate(rect, i, origin='centroid')
+        draw(shape=rls,color='yellow')
