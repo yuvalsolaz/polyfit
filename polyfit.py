1d4298659a690d04eb0d69c6d8e694f81321274c
yuvalsolaz <yuval.solaz@gmail.com>
2019-02-26 15:27:37 +0200

    scipy optimization

--- polyfit.py
+++ polyfit.py
@@ -19,19 +19,19 @@ fig, ax = plt.subplots(figsize=(5, 5))
 def draw(poly, rect):
     plt.cla()
     GeoSeries(poly).plot(ax=ax,color='green')
-    GeoSeries(rect).plot(ax=ax,color='yellow')
-    x = xor(poly=poly, rect=rect)
+    GeoSeries(rect).plot(ax=ax,color='blue')
+    x = diff(poly=poly, rect=rect)
     GeoSeries(x).plot(ax=ax,color='black')
     plt.pause(0.05)
 
 
-def xor(poly, rect='y'):
+def diff(poly, rect='y'):
     intersection = poly.intersection(rect)
     nonoverlap = rect.difference(intersection)
     return nonoverlap
 
 def loss(poly, rect):
-    return xor(poly,rect).area
+    return diff(poly, rect).area
 
 # calculate gradient numericly:
 def gradient(poly,rect):
@@ -48,6 +48,7 @@ loss_threshold_count = 40
 def sgd(poly,cannonical_rect):
     # initialize minimum loss with current loss
     min_loss = sys.float_info.max
+    min_loss_index = 0
 
     # initialize params :
     # x,y = aligned with polygon centroid
@@ -64,13 +65,13 @@ def sgd(poly,cannonical_rect):
         curr_loss = loss(poly, rect)
 
         # update minimum loss iteration index:
-        if curr_loss < min_loss:
-            min_loss_iteration = i
+        if (min_loss - curr_loss) > 1e-5 :
+            min_loss_index = i
             min_loss = curr_loss
 
         # check stopping condition
-        if curr_loss == 0 or i - min_loss_iteration > loss_threshold_count :
-            input(f'local minimum found. \n minimum loss:{min_loss} after {min_loss_iteration} iterations \n ok ?')
+        if curr_loss == 0 or i - min_loss_index > loss_threshold_count :
+            input(f'local minimum found. \n minimum loss:{min_loss} after {min_loss_index} iterations \n ok ?')
             break
 
         # calculate gradient :
@@ -78,7 +79,7 @@ def sgd(poly,cannonical_rect):
 
         # draw current position & current loss:
         draw(poly, rect)
-        print (f'iteration:{i} current loss:{curr_loss} min loss:{min_loss} min loss iteration:{min_loss_iteration} grad:{g}')
+        print (f'iteration:{i} current loss:{curr_loss} min loss:{min_loss} min loss iteration:{min_loss_index} grad:{g}')
 
         # update parameters x , y , alpha
         curr_params = Params(x = curr_params.x - learning_rate * g.x,
@@ -88,6 +89,26 @@ def sgd(poly,cannonical_rect):
         # translate rect:
         rect = affinity.translate(cannonical_rect, curr_params.x, curr_params.y)
         rect = affinity.rotate(rect, curr_params.alpha)
+
+import numpy as np
+from scipy.optimize import minimize
+
+def find_min(poly,rect):
+
+    def minimize_function(x):
+        # draw current position & current loss:
+        r = affinity.rotate(affinity.translate(rect, x[0], x[1]), x[2])
+        l = loss(poly,r)
+        print (f'x:{x[0]} {x[1]} {x[2]} loss:{l}')
+        draw(poly,r)
+
+
+        return loss(poly,affinity.rotate(affinity.translate(rect, x[0], x[1]), x[2]))
+
+    x0 =  np.array([poly.centroid.x - rect.centroid.x ,poly.centroid.y - rect.centroid.y ,0.0])
+    res = minimize(minimize_function, x0, method='nelder-mead',options={'xtol': 1e-8, 'disp': True})
+    print (f'minimized params: {res.x}')
+    return res
 
 
 if __name__ == '__main__':
@@ -102,4 +123,8 @@ if __name__ == '__main__':
 
     rect = loads(f'POLYGON(({-w} {-h}, {-w} {h}, {w} {h}, {w} {-h}, {-w} {-h}))')
 
-    sgd(poly,rect)
+    # sgd(poly,rect)
+
+    res = find_min(poly,rect)
+    input(f'minimize results:{res} \n ok ?')
+
